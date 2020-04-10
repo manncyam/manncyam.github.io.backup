bca670ca7b106da40685769ab0926590c7bfb673
manncyam <manncyam@gmail.com>
2020-04-10 04:37:45 -0700

    initialize a new site for portfolio

---
+++ precache-manifest.304476997212704125f2a8375986e582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670e76cd142302e2d7be39ec73f786b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9009d757184dde222fef",
+    "url": "/static/css/main.93a9b768.chunk.css"
+  },
+  {
+    "revision": "d87e1c19c8b7863f8776",
+    "url": "/static/js/2.c45d317d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c45d317d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9009d757184dde222fef",
+    "url": "/static/js/main.f529728e.chunk.js"
+  },
+  {
+    "revision": "7b193345e08b88368915",
+    "url": "/static/js/runtime-main.11d90e6e.js"
+  }
+]);
