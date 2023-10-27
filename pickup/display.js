311f59168f428d9b021667e1eb734f50a09c6f05
manncyam <manncyam@gmail.com>
2023-10-27 14:30:35 -0700

    use weblock to keep the screen on

--- pickup/display.js
+++ pickup/display.js
@@ -12,7 +12,14 @@ function show(e){
 }
 
 function edit(e){
-    console.log(e)
     document.getElementById('heroe-div').hidden = true;
     document.getElementById('message-form').hidden = false;
 }
+
+(() =>{
+document.addEventListener("visibilitychange", async () => {
+    if (wakeLock !== null && document.visibilityState === "visible") {
+      wakeLock = await navigator.wakeLock.request("screen");
+    }
+});
+})()
