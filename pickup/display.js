be36f3b32cf3958d6c37a92777950af123511cf1
manncyam <manncyam@gmail.com>
2023-10-27 10:38:26 -0700

    add bootstrap

--- pickup/display.js
+++ pickup/display.js
@@ -1,26 +1,16 @@
 function show(e){
     e.preventDefault();
     guest = document.getElementById('name');
+    document.getElementById('heroe-div').hidden = false;
     message = document.getElementById('message');
     message.innerHTML = guest.value;
-    message.hidden = false;
 
     document.getElementById('message-form').hidden = true;
-
-    edit_btn = document.getElementById('edit');
-    if (edit_btn.style.display === 'none') {
-        edit_btn.style.display = 'block';
-    }
-
     return false;
 }
 
 function edit(e){
-    if (e.style.display === 'block') {
-        e.style.display = 'none';
-    }
-    message = document.getElementById('message');
-    message.hidden = true;
+    document.getElementById('heroe-div').hidden = true;
 
     var message_form = document.getElementById('message-form');
     message_form.hidden = false;
