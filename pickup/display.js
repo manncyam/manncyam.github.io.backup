f03a9e4a3deaca717112ee22dd29706bf898e34f
manncyam <manncyam@gmail.com>
2023-10-27 14:49:07 -0700

    fixed wakeLock

--- pickup/display.js
+++ pickup/display.js
@@ -16,10 +16,22 @@ function edit(e){
     document.getElementById('message-form').hidden = false;
 }
 
-(() =>{
+// Create a reference for the Wake Lock.
+let wakeLock = null;
+
+// create an async function to request a wake lock
+try {
+  wakeLock = await navigator.wakeLock.request("screen");
+} catch (err) {
+  // The Wake Lock request has failed - usually system related, such as battery.
+  statusElem.textContent = `${err.name}, ${err.message}`;
+}
+
+wakeLock.addEventListener("release", () => {
+
+});
 document.addEventListener("visibilitychange", async () => {
     if (wakeLock !== null && document.visibilityState === "visible") {
       wakeLock = await navigator.wakeLock.request("screen");
     }
 });
-})()
