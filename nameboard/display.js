7aa3e1d16b40d3dd69568c2a89bdf5f5be47f36e
manncyam <manncyam@gmail.com>
2023-11-08 19:55:25 -0800

    Updates

---
+++ nameboard/display.js
@@ -0,0 +1,28 @@
+
+document.getElementById('heroe-div').hidden = true;
+var noSleep = new NoSleep();
+var message = document.getElementById('edit');
+
+function show(e){
+    e.preventDefault();
+    let guest = document.getElementById('name');
+    document.getElementById('heroe-div').hidden = false;
+    message.innerHTML = guest.value;
+
+    if (document.getElementById('is_neon').checked){
+        message.classList.add("neon")
+    }
+
+    document.getElementById('message-form').hidden = true;
+    
+    noSleep.enable();
+    return false;
+}
+
+function edit(e){
+    document.getElementById('heroe-div').hidden = true;
+    document.getElementById('message-form').hidden = false;
+    noSleep.disable();
+
+    message.classList.remove("neon")
+}
